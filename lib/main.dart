482ca204ca037c4ad79363905a266c398f418e9a
trinhtan1234 <trinhtan1703@gmail.com>
2023-09-12 19:29:23 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:luyencode/screen_action_button.dart';
+
+import 'textflieuchanle.dart';
 
 
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ScreenActionButton(),
+      home: const MyText(),
     );
   }
 }
--- lib/textflieuchanle.dart
+++ lib/textflieuchanle.dart
@@ -19,41 +19,44 @@ class _MyTextState extends State<MyText> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-           TextField(
-            onChanged: (text) {
-              _numberBloc.onTextFileChanged(text); // Truyền giá trị từ TextField vào hàm
-            },
-          ),
-          TextField(
-            onChanged: (text) {
-              _numberBloc.onTextFileChanged(text); // Truyền giá trị từ TextField vào hàm
-            },
-          ),
-          StreamBuilder<String?>(
-              stream: _numberBloc.stream,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Text(
-                    snapshot.error.toString(),
-                    style: const TextStyle(color: Colors.red),
-                  );
-                }
-                if (snapshot.hasData) {
-                  return Text(snapshot.data ?? '');
-                } else {
-                  return const Text('Ban chua nhap gi');
-                }
-              }),
-              TextButton(
-                onPressed: (){
-                  _numberBloc.counter(0);
-                }, 
-              child: const Icon(Icons.add),
-              ),
-             
-        ],
+      body: Container(
+        margin: const EdgeInsets.only(top: 200),
+        child: Column(
+          children: [
+             TextField(
+              onChanged: (text) {
+                _numberBloc.onTextFileChanged(text); // Truyền giá trị từ TextField vào hàm
+              },
+            ),
+            TextField(
+              onChanged: (text) {
+                _numberBloc.onTextFileChanged(text); // Truyền giá trị từ TextField vào hàm
+              },
+            ),
+            StreamBuilder<String?>(
+                stream: _numberBloc.stream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(
+                      snapshot.error.toString(),
+                      style: const TextStyle(color: Colors.red),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data ?? '');
+                  } else {
+                    return const Text('Ban chua nhap gi');
+                  }
+                }),
+                TextButton(
+                  onPressed: (){
+                    _numberBloc.counter(0);
+                  }, 
+                child: const Icon(Icons.add),
+                ),
+               
+          ],
+        ),
       ),
     );
   }
